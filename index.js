a1ba606927fe54526663d370c377e3c4a0f9de20
Parcival791 <66206896+Parcival791@users.noreply.github.com>
2024-01-02 15:08:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('button-add').addEventListener('click', function() {
+    // Öffne ein neues Fenster mit einer HTML-Datei
+    var newWindow = window.open('./popup/add.html', 'Neues Fenster', 'width=450,height=100');
+    
+  });
+  
